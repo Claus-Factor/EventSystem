8fa78467a6f6cd2b996827b19ee75531a27ed301
Nicholas <volxin.k2004@gmain.com>
2025-06-24 15:09:40 +0300

    Initial commit - base configuration

---
+++ event_generator/src/test/java/ru/nicholas/eventgenerator/EventgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nicholas.eventgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventgeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
